60f950cf8939760ae2c00b7d1f407e1ebf084478
carambis <lepeshko1997@mail.ru>
2018-10-11 21:59:03 +0300

    third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import {AppComponent} from './app.component';
 import {MainPageComponent} from './main-page/main-page.component';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
+import {SecongPageComponent} from './secong-page/secong-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SecongPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/secong-page/secong-page.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-secong-page',
+  templateUrl: './secong-page.component.html',
+  styleUrls: ['./secong-page.component.css']
+})
+export class SecongPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
